3bf6fda03c86a3f016c606b05f5ce528bfeac471
JotaPrimo <gestald118@gmail.com>
2024-01-27 11:07:53 -0400

    Controller
    - Criado Controller

--- src/main/java/br/com/jotasantos/crud/spring/web/controllers/UserController.java
+++ src/main/java/br/com/jotasantos/crud/spring/web/controllers/UserController.java
@@ -1,4 +1,25 @@
 package br.com.jotasantos.crud.spring.web.controllers;
 
+import br.com.jotasantos.crud.spring.services.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
 public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("users", userService.list());
+
+        return "users/index";
+    }
 }
