0ebc634f1033fa4acbea2f4288dae77c331094d5
JotaPrimo <gestald118@gmail.com>
2024-01-28 17:54:02 -0400

    Service
    - Adicionando novos métodos crud

--- src/main/java/br/com/jotasantos/crud/spring/services/UserService.java
+++ src/main/java/br/com/jotasantos/crud/spring/services/UserService.java
@@ -1,12 +1,15 @@
 package br.com.jotasantos.crud.spring.services;
 
 import br.com.jotasantos.crud.spring.entities.User;
+import br.com.jotasantos.crud.spring.exceptions.EntityNotFoundException;
 import br.com.jotasantos.crud.spring.repositories.UserRepository;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -31,4 +34,24 @@ public class UserService {
         return userRepository.save(user);
     }
 
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Transactional
+    public User delete(Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+
+        if (optionalUser.isPresent()) {
+            User userDel = optionalUser.get();
+            userDel.setAtivo(false);
+            userDel.setDeleteddAt(LocalDateTime.now());
+            return userDel;
+        }
+
+        throw new EntityNotFoundException(String.format("User of #%id", id));
+    }
+
+    public void update(Long id, User user) {
+    }
 }
