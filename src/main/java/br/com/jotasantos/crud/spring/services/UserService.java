fa203c2479f145c5947e52b2d7c1f703789b5c86
JotaPrimo <gestald118@gmail.com>
2024-01-27 23:08:39 -0400

    Service
    - Adicionado método store

--- src/main/java/br/com/jotasantos/crud/spring/services/UserService.java
+++ src/main/java/br/com/jotasantos/crud/spring/services/UserService.java
@@ -4,6 +4,7 @@ import br.com.jotasantos.crud.spring.entities.User;
 import br.com.jotasantos.crud.spring.repositories.UserRepository;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,8 +20,15 @@ public class UserService {
         return userRepository.findAll();
     }
 
+    @Transactional(readOnly = false)
     public List<User> findAllSorted() {
         Sort sort = Sort.by(Sort.Order.asc("name"));
         return userRepository.findAll(sort);
     }
+
+    @Transactional(readOnly = false)
+    public User store(User user) {
+        return userRepository.save(user);
+    }
+
 }
