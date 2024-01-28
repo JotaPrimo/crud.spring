bb357e62b147a2d8940b37d7bd1854c5184f8d95
JotaPrimo <gestald118@gmail.com>
2024-01-28 17:56:51 -0400

    Service
    - DataService para formatar datas

---
+++ src/main/java/br/com/jotasantos/crud/spring/services/DataService.java
@@ -0,0 +1,10 @@
+package br.com.jotasantos.crud.spring.services;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DataService {
+    public static void getDataAtualDMYHMS() {
+
+    }
+}
